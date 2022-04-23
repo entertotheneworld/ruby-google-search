1487af66d0425db4d790a4adf60e4969817d0652
entertotheneworld <tristan.greffe@hotmail.com>
2022-04-23 19:04:08 +0200

    Second version

--- google_searcher.rb
+++ google_searcher.rb
@@ -1,27 +1,21 @@
 require 'launchy'
 
-
-def google_searcher
-    research = ARGV
-
-    if research.length == 0 
-        puts "ERROR, no ARGV in"
-        puts "If you want to search on google: 'how to center a div', execute the following line:"
-        puts " > ruby google_searcher.rb how to center a div"
-    else 
-        url =  "https://www.google.com/search?q="
-        research.each_with_index {|word, i|
-            url += word + "+" if i < research.length - 1
-            url += word if i == research.length - 1
-        }
-        Launchy.open(url)
-    end
+def check_if_user_gave_input
+    abort("mkdir: missing input, try :\n > ruby google_searcher.rb how to center a div") if ARGV.empty?
 end
 
-google_searcher
+def create_url(argv)
+    url =  "https://www.google.com/search?q="
+    argv.each_with_index {|word, i|
+        url += word + "+" if i < argv.length - 1
+        url += word if i == argv.length - 1
+    }
+    return url
+end
 
+def perform
+    check_if_user_gave_input
+    Launchy.open(create_url(ARGV))
+end
 
-
-
-
-
+perform
